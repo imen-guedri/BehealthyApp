900fd0e91060c5ac12dab63f76368f75dcee6e87
Imen Kadri <imen.kadri@edu.isetcom.tn>
2026-03-15 23:04:42 +0100

    TP3 :Profile et navigation : BehealthyApp

--- app/src/main/java/com/example/behealthyapp/ProfileActivity.kt
+++ app/src/main/java/com/example/behealthyapp/ProfileActivity.kt
@@ -1,9 +1,18 @@
 package com.example.behealthyapp
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val btnRetour = findViewById<Button>(R.id.btnRetour)
+
+        btnRetour.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
